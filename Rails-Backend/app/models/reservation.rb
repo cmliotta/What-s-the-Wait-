4bfddff59d880ca11f4262e4d1db061cd6323405
cmliotta <cmliotta@msn.com>
2015-02-14 15:11:03 -0800

    adds reservation names

--- Rails-Backend/app/models/reservation.rb
+++ Rails-Backend/app/models/reservation.rb
@@ -3,4 +3,10 @@ class Reservation < ActiveRecord::Base
   belongs_to :patron
 
   validates_presence_of :restaurant_id, :patron_id, :party_size, :minutes
+  before_save :add_patron_name
+
+  def add_patron_name
+    self.first_name = self.patron.first_name
+    self.last_initial = self.patron.last_name[0]
+  end
 end
