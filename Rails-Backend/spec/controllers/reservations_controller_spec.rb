e8eb12e8a0dca595888d89f3a7a6625f64ec271f
cmliotta <cmliotta@msn.com>
2015-02-13 22:09:36 -0800

    adds 5 controller tests

--- Rails-Backend/spec/controllers/reservations_controller_spec.rb
+++ Rails-Backend/spec/controllers/reservations_controller_spec.rb
@@ -2,10 +2,14 @@ require 'rails_helper'
 
 describe ReservationsController  do
   before :each do
+
     Restaurant.delete_all
+
     @restaurant = Restaurant.create(first_name: "Chris", last_name: "Bo", restaurant_name: "Bennys", email: "a@a.com", city: "reno", state: "nv", zip:"2323", phone: "232", password: "hi")
 
     @reservation = Reservation.create(restaurant_id: 1, patron_id: 1, party_size: 4, minutes: 30)
+
+    @patron = Patron.create(first_name: "Jan", last_name:"Koller", cell_phone: "1313", email:"b@b.com", password: "1213")
   end
 
   describe "GET #index" do
@@ -16,54 +20,45 @@ describe ReservationsController  do
   end
 
   describe "#show" do
-    it "renders json" do
+    it "includes the wait time" do
       get(:show, restaurant_id: @restaurant.id, id: @reservation.id)
-      expect(response.body).to include("")
+      expect(response.body).to include("30")
     end
   end
 
-  # describe "#destroy" do
-  #   it "destroys the question" do
-  #     expect{
-  #       delete(:destroy, 'id' => question.id)
-  #     }.to change(Question, :count).by(-1)
-  #   end
-  # end
+  describe "#destroy" do
+    it "destroys the reservation" do
+      expect{
+        delete(:destroy, restaurant_id: @restaurant.id, id: @reservation.id)
+      }.to change(Reservation, :count).by(-1)
+    end
+  end
 
-  # describe "#create" do
-  #   it "creates the question" do
-  #     expect{
-  #       post(:create, question: {title: question.title, content: question.content})
-  #     }.to change(Question, :count).by(1)
-  #   end
-  # end
+  describe "#create" do
+    it "creates a new reservation" do
+      expect{
+        post(:create, restaurant_id: 2, reservation: {restaurant_id: 2, patron_id: 1, party_size: 3, minutes: 10})
+      }.to change(Reservation, :count).by(1)
+    end
+  end
 
-  # describe "#edit" do
-  #   it "renders edit template" do
-  #     get(:edit, {'id' => question.id})
-  #     expect(response).to render_template(:edit)
-  #   end
-  # end
+  describe "#update" do
+    it "updates the reservation" do
+      put :update, restaurant_id: @restaurant.id, id: @reservation.id, reservation: { party_size: 3, minutes: 40 }
+      @reservation.reload
+      expect(@reservation.minutes).to eq(40)
+    end
+  end
 
-  # describe "#update" do
-  #   it "changes the title of the question" do
-  #     # attrs = {title: "new title", content: "new content"}
-  #     # put :update, 'id' => question.id, question: attrs
-  #     put :update, id: question.id, question: { title: "new title", content: "new content" }
-  #     question.reload
-  #     expect(question.title).to eq("new title")
-  #   end
-  # end
+  describe "#add_time" do
+    it "adds 5 minutes to " do
+      initial = question.vote_count
+      post :upvote, id: question.id
+      question.reload
 
-  # describe "#upvote" do
-  #   it "incements the vote_count by 1 when action is hit" do
-  #     initial = question.vote_count
-  #     post :upvote, id: question.id
-  #     question.reload
-
-  #     expect(question.vote_count).to eq(initial+1)
-  #   end
-  # end
+      expect(question.vote_count).to eq(initial+1)
+    end
+  end
 
   # describe "#downvote" do
   #   it "decements the vote_count by 1 when action is hit" do
